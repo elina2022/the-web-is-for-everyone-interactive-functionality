4726e3ce56245e0741cb3b1b36a3fbca2bee22e7
elina2022 <elinakniazian@gmail.com>
2023-04-07 01:29:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.set("view engine", "ejs")
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
+//logs when request is made
 app.use((req, res, next) => {
     console.log("Request made on following url: " + req.url)
     next()
@@ -18,15 +19,13 @@ try {
     const data1 = getData("game/943")
     const data2 = getData("game/943/statistics")
     const players = getData("facts/players-extra")
-    // const player1 = getData("facts/Player/8602")
-    // const player2 = getData("facts/Player/8607")
-    // const player3 = getData("facts/Player/17373")
     data = Object.assign({}, data1, data2, players)
     console.log(data) 
 } catch (err) {
     console.error(err) 
 }
 
+//rendering pages
 app.get('/', async (req, res) => {
     res.render('index', data)
 })
@@ -34,24 +33,3 @@ app.get('/', async (req, res) => {
 app.use((req, res) => {
     res.status(404).render("404")
 })
-
-
-
-
-// let jsonData1
-// let jsonData2
-// try {
-//     const data1 = fs.readFileSync('./public/api/game/943.json')
-//     jsonData1 = JSON.parse(data1.toString())
-//     console.log(jsonData1)
-    
-//     const data2 = fs.readFileSync('./public/api/game/943/statistics.json')
-//     jsonData2 = JSON.parse(data2.toString())
-//     console.log(jsonData2)
-// } catch (err) {
-//     console.error(err);
-// }
-
-
-
-// && fs.existsSync("/api/facts/Player/" + player.playerId + ".json")
